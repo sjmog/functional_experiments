1292b3c0dcacffd1f471bd038aa8095ea4d71691
Sam Morgan <s_morgan@me.com>
2018-08-14 15:56:12 +0100

    Add some comments for instructional clarity

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//// SETUP
+// immutable incoming data. We don't want to mutate this.
 const DATA = [
   {
     "type": "Post",
@@ -20,10 +22,18 @@ const DATA = [
   }
 ]
 
+// a function to process a pipeline of functions, passing the data through each one.
 const processPipeline = (data, fns) => fns.reduce((data, fn) => fn(data), data)
 
+// a function to avoid mutating the data within functions. 
+// instead, copy the data with modifications, which are supplied as rules.
 const copy = (object, rule) => Object.assign({}, object, rule(object))
 
+// rules for modifying data.
+// to add a new modification: 
+//   * write a rule, 
+//   * create a new rule pattern, and 
+//   * add the pattern to the pipeline.
 const lowercaseRule = (object) => {
   return {
     user: {
@@ -47,15 +57,23 @@ const addOneRule = (object) => {
   }
 }
 
+// rule patterns. These decide how rules are applied.
+// in general, you probably want to copy the data one modified object at a time.
 const lowercaseNames = (data) => data.map(object => copy(object, lowercaseRule))
 const uppercaseText  = (data) => data.map(object => copy(object, uppercaseRule))
 const addOneToIds    = (data) => data.map(object => copy(object, addOneRule))
 
+// another function that can be inserted into the pipeline.
+// here, this is just an example function that notifies the developers.
+// like all pipeline functions, it has to return the data to the next part of the pipeline.
 const notifyFakeDevs = (data) => {
-  console.log("Now processing batch 1 of 14...")
+  console.log("Now processing", data)
   return data
 }
 
+//// EXECUTION
+// after all the setup, run the pipeline.
 let processedData = processPipeline(DATA, [lowercaseNames, uppercaseText, addOneToIds, notifyFakeDevs])
-console.log(processedData)
-console.log(DATA)
+
+console.log('the data going into the pipeline, which has not been mutated\n', DATA)
+console.log('the data coming out of the pipeline\n', processedData)
